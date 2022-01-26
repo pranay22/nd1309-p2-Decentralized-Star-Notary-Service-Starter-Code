51c86a8454bf8f6dd53747d0b093b9a5b3ea20a3
Pranay Sarkar <pranaysarkar22@gmail.com>
2022-01-26 19:19:43 +0100

    na: create star test done and success [Pranay Sarkar] [V]

--- test/TestStarNotary.js
+++ test/TestStarNotary.js
@@ -77,7 +77,11 @@ it('lets user2 buy a star and decreases its balance in ether', async() => {
 
 it('can add the star name and star symbol properly', async() => {
     // 1. create a Star with different tokenId
+    let starId = 6;
+    await instance.createStar('awesome star', starId, { from: user1 });
     //2. Call the name and symbol properties in your Smart Contract and compare with the name and symbol provided
+    assert.equal('Pranay Star Token', await instance.name());
+    assert.equal('PST', await instance.symbol());
 });
 
 it('lets 2 users exchange stars', async() => {
